6b0efb76aa2497368b04c7d71ea1f280c51989f2
Tony Juntao Hu <54758881+tonyhu-x@users.noreply.github.com>
2024-05-16 17:47:13 -0400

    Add React

--- eslint.config.js
+++ eslint.config.js
@@ -2,12 +2,14 @@ import globals from 'globals';
 import pluginJs from '@eslint/js';
 import tseslint from 'typescript-eslint';
 import reactRecommended from 'eslint-plugin-react/configs/recommended.js';
+import reactJsxRuntime from 'eslint-plugin-react/configs/jsx-runtime.js';
 import stylistic from '@stylistic/eslint-plugin';
 
 export default [
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
   reactRecommended,
+  reactJsxRuntime,
   stylistic.configs.customize({ semi: true }),
   {
     languageOptions: { globals: globals.browser },
