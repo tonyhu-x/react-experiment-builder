bb7349246a1d13510162893b8b84ac83217f68cc
Tony Juntao Hu <54758881+tonyhu-x@users.noreply.github.com>
2024-05-20 11:36:33 -0400

    Update Jest and ESLint configs

--- eslint.config.js
+++ eslint.config.js
@@ -4,6 +4,7 @@ import tseslint from 'typescript-eslint';
 import reactRecommended from 'eslint-plugin-react/configs/recommended.js';
 import reactJsxRuntime from 'eslint-plugin-react/configs/jsx-runtime.js';
 import stylistic from '@stylistic/eslint-plugin';
+import jest from 'eslint-plugin-jest';
 
 export default [
   pluginJs.configs.recommended,
@@ -12,14 +13,14 @@ export default [
   reactJsxRuntime,
   stylistic.configs.customize({ semi: true }),
   {
-    languageOptions: { globals: globals.browser },
-    overrides: [
-      {
-        files: ['tests/**/*'],
-        env: {
-          jest: true,
-        },
-      },
+    files: [
+      'src/**/*.test.ts',
+      'src/**/*.test.tsx',
     ],
+    ...jest.configs['flat/recommended'],
+    ...jest.configs['flat/style'],
+  },
+  {
+    languageOptions: { globals: globals.browser },
   },
 ];
